b3bcd1fe22687585a8c562f6f02f47cda1fb5bed
prana <prakashrana9847067614@gmail.com>
2020-04-06 20:18:58 -0400

    Initial commit

---
+++ app/src/main/java/com/example/c0773839_ex6/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.c0773839_ex6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 5000);
+    }
+}
